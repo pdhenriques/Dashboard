0b87a8d6fc423d50ea703dc33eca4ba0daeb3c13
Pedro Henriques <pdhenriques@gmail.com>
2020-03-16 18:45:17 +0100

    Add dummy menu

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,27 @@ import React from "react";
 import "./App.css";
 import Dashboard from "./Dashboard";
 
-
 class App extends React.Component {
-
     render() {
         return (
-            <div className="ui segment">
-                <Dashboard />
+            <div>
+                <div class="ui fixed inverted menu">
+                    <div class="ui container">
+                        <a href="#" class="header item">
+                            Custom Dashboard
+                        </a>
+                        <a href="#" class="item">
+                            Home
+                        </a>
+                        <a href="#" class="item">
+                            About
+                        </a>
+                    </div>
+                </div>
+                <br />
+                <div className="ui segment">
+                    <Dashboard />
+                </div>
             </div>
         );
     }
